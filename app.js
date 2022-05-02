efd56c2c6a3e57caaf94903a12855eb550670bcc
fadedrifleman <fadedrifleman@gmail.com>
2022-05-02 18:30:28 +0530

    username and password only

--- app.js
+++ app.js
@@ -9,13 +9,61 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://localhost:27017/userDB");
 
+const userSchema = {
+    _id: String,
+    email: String,
+    password: String
+};
 
+const User = new mongoose.model("User", userSchema);
 
+app.get("/", (req, res) => {
+    res.render("home");
+});
 
+app.get("/login", (req, res) => {
+    res.render("login");
+});
 
+app.get("/register", (req, res) => {
+    res.render("register");
+});
 
+app.post("/register", (req, res) => {
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+    newUser.save((err) => {
+        if(err) {
+            console.log(err);
+        } else {
+            res.render("secrets");
+        }
+    });
+});
 
+app.post("/login", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    User.findOne({email: username}, (err, foundUser) => {
+        if(err) {
+            console.log(err);
+        } else{
+            if(foundUser) {
+                if(foundUser.password === password) {
+                    res.render("secrets");
+                }
+            }
+        }
+    });
+});
+
+app.get("/logout", (req, res) => {
+    res.render("home");
+});
 
 
 
