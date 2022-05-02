1e83bceb4b06b5deb8c76c17d9daa05539dfe1d2
fadedrifleman <fadedrifleman@gmail.com>
2022-05-02 20:08:20 +0530

    added dotenv

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const bodyParser = require('body-parser');
 const express = require('express');
 const mongoose = require('mongoose');
@@ -17,8 +18,7 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-const secret = "ThisIsOurEncryptionSecret";
-userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"]});
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
 
 const User = new mongoose.model("User", userSchema);
 
